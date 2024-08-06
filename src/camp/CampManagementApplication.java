e301e31308cbd36f5fcd7f663469c6e92f2b7579
mj-song00 <niveus128@gmail.com>
2024-08-06 15:49:22 +0900

    Chore: 입력값 수정

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -234,10 +234,10 @@ public class CampManagementApplication {
         // 선택과목 등록
         ArrayList<String> choiceArr = new ArrayList<>();
         System.out.println("2개 이상의 선택 과목을 입력해주세요: ");
+        sc.nextLine();
 
         while (true) {
             String choice = sc.nextLine();
-            sc.nextLine();
 
             boolean isChoiceExist = choiceArr.contains(choice);
 
@@ -281,17 +281,20 @@ public class CampManagementApplication {
         System.out.println("수강생 등록 성공!\n");
 
     }
-        // 수강생 목록 조회
-        private static void inquireStudent () {
-            System.out.println("\n수강생 목록을 조회합니다...");
-            for (Student student : studentStore) {
-                int num = 1;
-                System.out.println(num + " 1) 수강생 이름 : " + student.getStudentName() + "\n  2) 수강생 고유번호 : " + student.getStudentId()
-                        + "\n  3) 수강 과목 : [필수 - " + student.getMandatorySubjects() + "]" + " [선택 - " + student.getChoiceSubjects() + "]");
-                num++;
-            }
-            System.out.println("\n수강생 목록 조회 성공!");
+
+    // 수강생 목록 조회
+    private static void inquireStudent() {
+        System.out.println("\n수강생 목록을 조회합니다...");
+        for (Student student : studentStore) {
+            int num = 1;
+            System.out.println(num + " 1) 수강생 이름 : " + student.getStudentName() + "\n  2) 수강생 고유번호 : " + student.getStudentId()
+                    + "\n  3) 수강 과목 : [필수 - " + student.getMandatorySubjects() + "]" + " [선택 - " + student.getChoiceSubjects() + "]");
+            num++;
         }
+        System.out.println("\n수강생 목록 조회 성공!");
+    }
+
+
 
         private static void displayScoreView () {
             boolean flag = true;
@@ -324,9 +327,9 @@ public class CampManagementApplication {
         }
 
 
-        /*---------------------------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------------------*/
 
-        // 수강생의 과목별 시험 회차 및 점수 등록
+    // 수강생의 과목별 시험 회차 및 점수 등록
         private static void createScore () {
             String studentId = getStudentId(); // 관리할 수강생 고유 번호
             System.out.println("시험 점수를 등록합니다...");
@@ -389,9 +392,9 @@ public class CampManagementApplication {
             System.out.println("\n점수 등록 성공!");
         }
 
-        /*---------------------------------------------------------------------------------------*/
+    /*---------------------------------------------------------------------------------------*/
 
-        // 수강생의 과목별 회차 점수 수정
+    // 수강생의 과목별 회차 점수 수정
         private static void updateRoundScoreBySubject () {
             String studentId = getStudentId(); // 관리할 수강생 고유 번호
             // 기능 구현 (수정할 과목 및 회차, 점수)
@@ -408,6 +411,7 @@ public class CampManagementApplication {
             // 기능 구현
             System.out.println("\n등급 조회 성공!");
         }
-    }
+
+}
 
 
