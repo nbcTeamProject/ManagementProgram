b812e83baf90b872d46b8102b54a23fbd737f876
mj-song00 <niveus128@gmail.com>
2024-08-07 13:05:49 +0900

    feat: StudentService 분리

--- src/camp/CampManagementApplication.java
+++ src/camp/CampManagementApplication.java
@@ -4,7 +4,7 @@ package camp;
 import camp.model.Score;
 import camp.model.Student;
 import camp.model.Subject;
-
+import camp.service.StudnetService;
 import java.util.*;
 
 /**
@@ -104,7 +104,7 @@ public class CampManagementApplication {
     }
 
     // index 자동 증가
-    private static String sequence(String type) {
+    public static String sequence(String type) {
         switch (type) {
             case INDEX_TYPE_STUDENT -> {
                 studentIndex++;
@@ -171,114 +171,115 @@ public class CampManagementApplication {
 
     // 수강생 등록
     private static void createStudent() {
-        System.out.println("\n수강생을 등록합니다...");
-        System.out.print("수강생 이름 입력: ");
-        String studentName = sc.next();
-        // 기능 구현 (필수 과목, 선택 과목)
 
-        //과목 가져오기
-        Map<String, String> subjects = new HashMap();
-        for (Subject subject : subjectStore) {
-            subjects.put(subject.getSubjectName(), subject.getSubjectType());
-        }
-        System.out.println(subjects);
-
-        //필수과목 등록
-        ArrayList<String> mandatoryArr = new ArrayList<>();
-        System.out.println("3개 이상의 필수 과목을 입력해주세요: ");
-        sc.nextLine();
-
-        while (true) {
-            String essential = sc.nextLine();
-            System.out.println(subjects.get(essential));
-
-            boolean isExist = mandatoryArr.contains(essential);
-
-            // 필수과목 입력
-            // essential이 필수과목이 아닌경우
-            if (subjects.get(essential) != "MANDATORY") {
-                System.out.println("필수과목이 아닙니다.");
-
-                //입력값이 강의 목록에 없는 경우
-            } else if (!subjects.containsKey(essential)) {
-                System.out.println("입력값이 강의목록에 없습니다. 다시 입력해주세요.");
-
-                //이미 수강신청을 한 경우
-            } else if (isExist) {
-                System.out.println("이미 수강신청한 과목입니다.");
-                mandatoryArr.remove(mandatoryArr.size() - 1);
-
-            } else {
-                mandatoryArr.add(essential);
-            }
-
-
-            System.out.println(mandatoryArr);
-
-
-            if (mandatoryArr.size() >= 5) {
-                System.out.println("더이상 신청할 수 없습니다.");
-                break;
-            }
-
-            if (mandatoryArr.size() == 3) {
-                System.out.println("입력을 끝내겠습니까?(exit 입력시 종료) : ");
-                sc.nextLine();
-                String end = sc.next();
-                if (Objects.equals(end, "exit")) {
-                    System.out.println("입력을 종료합니다.");
-                    break;
-                }
-            }
-        }
-        // 선택과목 등록
-        ArrayList<String> choiceArr = new ArrayList<>();
-        System.out.println("2개 이상의 선택 과목을 입력해주세요: ");
-        sc.nextLine();
-
-        while (true) {
-            String choice = sc.nextLine();
-
-            boolean isChoiceExist = choiceArr.contains(choice);
-
-            // 선택과목 입력
-            // choice가 선택과목이 아닌경우
-            if (subjects.get(choice) != "CHOICE") {
-                System.out.println("선택과목이 아닙니다.");
-                //입력값이 강의 목록에 없는 경우
-            } else if (!subjects.containsKey(choice)) {
-                System.out.println("입력값이 강의목록에 없습니다. 다시 입력해주세요.");
-                //이미 수강신청을 한 경우
-            } else if (isChoiceExist) {
-                System.out.println("이미 수강신청한 과목입니다.");
-                choiceArr.remove(choiceArr.size() - 1);
-            } else {
-                choiceArr.add(choice);
-            }
-            if (choiceArr.size() == 4) {
-                System.out.println("더이상 신청할 수 없습니다.");
-                break;
-            }
-            System.out.println(choiceArr);
-
-            if (choiceArr.size() == 2) {
-                System.out.println("입력을 끝내겠습니까?(exit 입력시 종료) : ");
-                sc.nextLine();
-                String end = sc.next();
-                if (Objects.equals(end, "exit")) {
-                    System.out.println("입력을 종료합니다.");
-                    break;
-                }
-            }
-
-        }
-        String studentId = sequence(INDEX_TYPE_STUDENT);
-        Student student = new Student(studentId, studentName, mandatoryArr, choiceArr); // 수강생 인스턴스 생성 예시 코드
-        // 기능 구현
-        studentStore.add(student);
-        System.out.println(studentStore.size());
-
-        System.out.println("수강생 등록 성공!\n");
+//        System.out.println("\n수강생을 등록합니다...");
+//        System.out.print("수강생 이름 입력: ");
+//        String studentName = sc.next();
+//        // 기능 구현 (필수 과목, 선택 과목)
+//
+//        //과목 가져오기
+//        Map<String, String> subjects = new HashMap();
+//        for (Subject subject : subjectStore) {
+//            subjects.put(subject.getSubjectName(), subject.getSubjectType());
+//        }
+//        System.out.println(subjects);
+//
+//        //필수과목 등록
+//        ArrayList<String> mandatoryArr = new ArrayList<>();
+//        System.out.println("3개 이상의 필수 과목을 입력해주세요: ");
+//        sc.nextLine();
+//
+//        while (true) {
+//            String essential = sc.nextLine();
+//            System.out.println(subjects.get(essential));
+//
+//            boolean isExist = mandatoryArr.contains(essential);
+//
+//            // 필수과목 입력
+//            // essential이 필수과목이 아닌경우
+//            if (subjects.get(essential) != "MANDATORY") {
+//                System.out.println("필수과목이 아닙니다.");
+//
+//                //입력값이 강의 목록에 없는 경우
+//            } else if (!subjects.containsKey(essential)) {
+//                System.out.println("입력값이 강의목록에 없습니다. 다시 입력해주세요.");
+//
+//                //이미 수강신청을 한 경우
+//            } else if (isExist) {
+//                System.out.println("이미 수강신청한 과목입니다.");
+//                mandatoryArr.remove(mandatoryArr.size() - 1);
+//
+//            } else {
+//                mandatoryArr.add(essential);
+//            }
+//
+//
+//            System.out.println(mandatoryArr);
+//
+//
+//            if (mandatoryArr.size() >= 5) {
+//                System.out.println("더이상 신청할 수 없습니다.");
+//                break;
+//            }
+//
+//            if (mandatoryArr.size() == 3) {
+//                System.out.println("입력을 끝내겠습니까?(exit 입력시 종료) : ");
+//                sc.nextLine();
+//                String end = sc.next();
+//                if (Objects.equals(end, "exit")) {
+//                    System.out.println("입력을 종료합니다.");
+//                    break;
+//                }
+//            }
+//        }
+//        // 선택과목 등록
+//        ArrayList<String> choiceArr = new ArrayList<>();
+//        System.out.println("2개 이상의 선택 과목을 입력해주세요: ");
+//        sc.nextLine();
+//
+//        while (true) {
+//            String choice = sc.nextLine();
+//
+//            boolean isChoiceExist = choiceArr.contains(choice);
+//
+//            // 선택과목 입력
+//            // choice가 선택과목이 아닌경우
+//            if (subjects.get(choice) != "CHOICE") {
+//                System.out.println("선택과목이 아닙니다.");
+//                //입력값이 강의 목록에 없는 경우
+//            } else if (!subjects.containsKey(choice)) {
+//                System.out.println("입력값이 강의목록에 없습니다. 다시 입력해주세요.");
+//                //이미 수강신청을 한 경우
+//            } else if (isChoiceExist) {
+//                System.out.println("이미 수강신청한 과목입니다.");
+//                choiceArr.remove(choiceArr.size() - 1);
+//            } else {
+//                choiceArr.add(choice);
+//            }
+//            if (choiceArr.size() == 4) {
+//                System.out.println("더이상 신청할 수 없습니다.");
+//                break;
+//            }
+//            System.out.println(choiceArr);
+//
+//            if (choiceArr.size() == 2) {
+//                System.out.println("입력을 끝내겠습니까?(exit 입력시 종료) : ");
+//                sc.nextLine();
+//                String end = sc.next();
+//                if (Objects.equals(end, "exit")) {
+//                    System.out.println("입력을 종료합니다.");
+//                    break;
+//                }
+//            }
+//
+//        }
+//        String studentId = sequence(INDEX_TYPE_STUDENT);
+//        Student student = new Student(studentId, studentName, mandatoryArr, choiceArr); // 수강생 인스턴스 생성 예시 코드
+//        // 기능 구현
+//        studentStore.add(student);
+//        System.out.println(studentStore.size());
+//
+       System.out.println("수강생 등록 성공!\n");
 
     }
 
@@ -411,6 +412,23 @@ public class CampManagementApplication {
             // 기능 구현
             System.out.println("\n등급 조회 성공!");
         }
+
+    //getter
+    public static String getINDEX_TYPE_STUDENT(){
+        return INDEX_TYPE_STUDENT;
+    }
+    public static String getINDEX_TYPE_SUBJECT(){
+        return INDEX_TYPE_SUBJECT;
+    }
+    public static String getINDEX_TYPE_SCORE(){
+        return INDEX_TYPE_SCORE;
+    }
+    public static String getSUBJECT_TYPE_MANDATORY(){
+        return SUBJECT_TYPE_MANDATORY;
+    }
+    public static String getSUBJECT_TYPE_CHOICE(){
+        return SUBJECT_TYPE_CHOICE;
+    }
 
 }
 
